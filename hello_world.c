b73e5be0492e789eac4e12f65077e7ba5418a1ff
rlsl0422 <rlsl0422@gmail.com>
2022-11-21 23:22:10 +0900

    hello_wold

--- hello_world.c
+++ hello_world.c
@@ -6,6 +6,15 @@ int main(){
   {
     printf("%d\n",i*10);
   }
+  for (int i = 0; i < 10; i++)
+  {
+    if (i%2 != 1)
+    {
+      printf("%d\n",i);
+    }
+    
+  }
+  
   printf("end");
   return 0;
 }
